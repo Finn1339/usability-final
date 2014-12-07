c98af813befa45c06eb66d5f5bc9b4106d4e424a
stanleyrya <stanleyrya@gmail.com>
2014-12-07 02:27:33 -0500

    sidebar fun

--- sessions/public/sidebar.js
+++ sessions/public/sidebar.js
@@ -2,35 +2,61 @@
 $( document ).ready(function() {
 $('#sidebar li.has-sub>a').on('click', function(){
     $(this).removeAttr('href');
-
     var element = $(this).parent('li');
-
     if (element.hasClass('open')) {
-
       element.removeClass('open');
-
       element.find('li').removeClass('open');
-
       element.find('ul').slideUp();
-
     }
-
     else {
-
       element.addClass('open');
-
       element.children('ul').slideDown();
-
       element.siblings('li').children('ul').slideUp();
-
       element.siblings('li').removeClass('open');
-
       element.siblings('li').find('li').removeClass('open');
-
       element.siblings('li').find('ul').slideUp();
-
     }
-
   });
+});
+} )( jQuery );
+
+//this is for opening things
+( function( $ ) {
+$( document ).ready(function() {
+    $('#current').removeAttr('href');
+    var element = $('#current');
+    if (element.hasClass('open')) {
+      element.removeClass('open');
+      element.find('li').removeClass('open');
+      element.find('ul').slideUp();
+    }
+    else {
+      element.addClass('open');
+      element.children('ul').slideDown();
+      element.siblings('li').children('ul').slideUp();
+      element.siblings('li').removeClass('open');
+      element.siblings('li').find('li').removeClass('open');
+      element.siblings('li').find('ul').slideUp();
+    }
+});
+} )( jQuery );
+
+( function( $ ) {
+$( document ).ready(function() {
+    $('#current2').removeAttr('href');
+    var element = $('#current2');
+    if (element.hasClass('open')) {
+      element.removeClass('open');
+      element.find('li').removeClass('open');
+      element.find('ul').slideUp();
+    }
+    else {
+      element.addClass('open');
+      element.children('ul').slideDown();
+      element.siblings('li').children('ul').slideUp();
+      element.siblings('li').removeClass('open');
+      element.siblings('li').find('li').removeClass('open');
+      element.siblings('li').find('ul').slideUp();
+    }
 });
 } )( jQuery );
